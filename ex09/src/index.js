54030ca0e9ed7faa9ef3caa08accbf561517b4d3
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-25 09:20:59 +0100

    Added ex09 src and index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,43 @@
+const _= require('lodash');
+
+const users = [
+    { id: '1', firstName: 'John' lastName: 'Doe', age: 24, gender: 'male' },
+    { id: '2', firstName: 'Jane' lastName: 'Doe', age: 5, gender: 'female' },
+    { id: '3', firstName: 'Jim' lastName: 'Carrey', age: 54, gender: 'male' },
+    { id: '4', firstName: 'Kate' lastName: 'Winslet', age: 40, gender: 'female' }
+   
+   
+];
+
+
+
+function getUsers() {
+    var output = [""];
+for()
+    console.log(getUsers);
+    return output;
+}
+
+
+
+
+//findUser(lastName, gender) function starts here 
+function findUserById(id) {
+    try {
+        var user = _.find(user, {name: 'John', gender: 'male' });
+
+        var iFindUser = 
+        return iFindUser;
+
+    }   
+        
+    } catch (error) {
+        console.log(findUser); 
+        return "Cannot read property 'firstName' of undefined" 
+        
+    }jim
+
+}
+getUsers(output);
+findUserById(); 
+module.exports = findUserById;
