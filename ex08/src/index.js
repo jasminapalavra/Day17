4b2723aa25ee298999506708d24b31ac79672f80
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-25 09:19:34 +0100

    Added ex08 src and index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,42 @@
+const _= require('lodash');
+
+const users = [
+    { name: 'John', lastName: 'Doe', age: 24, gender: 'male' },
+    { name: 'Jane', lastName: 'Doe', age: 5, gender: 'female' },
+    { name: 'Jim', lastName: 'Carrey', age: 54, gender: 'male' },
+    { name: 'Kate', lastName: 'Winslet', age: 40, gender: 'female' }
+   
+];
+
+
+
+function getUsers() {
+    var output = [""];
+for()
+    console.log(getUsers);
+    return output;
+}
+
+
+
+
+//findUser(lastName, gender) function starts here 
+function findUser(lastName, gender) {
+    try {
+        var user = _.find(user, {name: 'John', gender: 'male' });
+
+        var iFindUser = 
+        return iFindUser;
+
+    }   
+        
+    } catch (error) {
+        console.log(findUser); 
+        return "Cannot read property 'firstName' of undefined" 
+        
+    }
+
+}
+getUsers(output);
+findUser(); 
+module.exports = findUser;
